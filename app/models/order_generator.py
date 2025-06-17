ee649a2ef4c47a5d03b4805c3cf0c89fe5db4b23
Lê Nhật Anh <lnafncy20@gmail.com>
2025-06-17 21:22:27 +0700

    Update order_generator.py

--- app/models/order_generator.py
+++ app/models/order_generator.py
@@ -2,18 +2,25 @@ import uuid
 import random
 from datetime import datetime, timezone
 
-PAYMENT_METHODS = ["Credit Card", "PayPal", "Cash", "Bank Transfer"]
+PAYMENT_METHODS = ["Credit Card", "PayPal", "COD", "Bank Transfer"]
+SHIPPING_METHODS = ["Free", "Standard", "Fast Express", " Next Day Shipping"]
 
-def generate_order(user, product):
+def generate_order(user, product, address):
     return {
         "order_id": str(uuid.uuid4()),
         "user_id": user["user_id"],
         "user_name": user["name"],
         "user_email": user["email"],
+        "street": address["street"],
+        "city": address["city"],
+        "state": address["state"],
+        "postal_code": address["postal_code"],
+        "country": address["country"],
         "product_id": product["product_id"],
         "category": product["category"],
         "price": product["price"],
         "quantity": random.randint(1, 5),
+        "shipping_method": random.choice(SHIPPING_METHODS),
         "payment_method": random.choice(PAYMENT_METHODS),
-        "timestamp": datetime.now(timezone.utc).isoformat()
+        "timestamp": datetime.now(timezone.utc).isoformat(),    
     }
