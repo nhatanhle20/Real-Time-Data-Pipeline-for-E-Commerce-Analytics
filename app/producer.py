ad1483aa1df9b9ece20f69363b31c8630b7606c3
Lê Nhật Anh <lnafncy20@gmail.com>
2025-06-22 01:38:27 +0700

    Update producer.py

--- app/producer.py
+++ app/producer.py
@@ -8,7 +8,7 @@ from models.order_generator import generate_order
 from models.address import generate_addresses, load_us_cities
 
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers="kafka:29092", # "localhost:9092" to run producer.py on local machine; "kafka:29092" to run on airflow 
     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     retries=5,
     max_request_size=1_000_000
@@ -23,13 +23,16 @@ addresses = generate_addresses(n=50, city_state_pool=city_state_pool)
 
 
 if __name__ == "__main__":
-    while True:
+    start_time = time.time()
+    run_duration = 60  # run for 60 seconds
+
+    while time.time() - start_time < run_duration:
         user = random.choice(users)
         product = random.choice(products)
         address = random.choice(addresses)
-        
+
         order = generate_order(user, product, address)
         print(f"Sending: {order}")
         producer.send("ecommerce-transactions", order)
         producer.flush()
-        time.sleep(1)
+        time.sleep(2)
