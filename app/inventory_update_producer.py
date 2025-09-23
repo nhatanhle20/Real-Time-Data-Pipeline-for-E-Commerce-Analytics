c0f4c4a6ba8f2067323db563af7731e25b092ac4
Lê Nhật Anh <lnafncy20@gmail.com>
2025-09-23 16:29:57 +0700

    Update inventory_update_producer.py

--- app/inventory_update_producer.py
+++ app/inventory_update_producer.py
@@ -4,25 +4,31 @@ import random
 import uuid
 from kafka import KafkaProducer
 from datetime import datetime, timezone
+from models.products import generate_products
+
+products = generate_products()
 
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers="kafka:29092", # "localhost:9092" to run on local machine; "kafka:29092" to run on airflow 
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-products = ["P001", "P002", "P003", "P004", "P005"]
+change_type = ["sold", "restock"]
 
 if __name__ == "__main__":
     start_time = time.time()
     run_duration = 60  # run for 60 seconds
-    
+    product = random.choice(products)
+
     while time.time() - start_time < run_duration:
         inventory_event = {
             "event_id": str(uuid.uuid4()),
-            "product_id": random.choice(products),
-            "change": random.randint(-100, 100),  # -20 means sold, +50 means restocked
+            "product_id": product["product_id"],
+            "change_type": random.choice(change_type),
+            "quantity_change": random.randint(0, 100),  
             "timestamp": datetime.now(timezone.utc).isoformat()
         }
         print(f"Sending inventory update: {inventory_event}")
         producer.send("inventory-updates", inventory_event)
+        producer.flush()
         time.sleep(random.uniform(1, 5))
